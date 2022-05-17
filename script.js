f983917ce2f56a17880a552914cf35711ad62db3
Trevor Tomlin <32802101+trevortomlin@users.noreply.github.com>
2022-05-17 16:45:12 -0700

    Updated website
    -Changed color pallet
    -Project section now pulls from github api
    -Fixed sidebar moving 
    -Added events section
    -Changed welcome text

--- script.js
+++ script.js
@@ -1,5 +1,17 @@
+const GITHUB_API_REPO_URL="https://api.github.com/users/trevortomlin/repos"
+
 $(document).ready(function () {
     
+	window.ondragstart = function() {return false}
+
+	repos = $.getJSON(GITHUB_API_REPO_URL, function( data ) {
+
+		$.each( data, function(id, obj) {
+			makeRepoCards(obj);
+		  });
+
+	});
+
 	$("#sideBarCollapse").click(function() {
 
 		$('#sidebar').toggleClass('collapsed');
@@ -25,6 +37,49 @@ $(document).ready(function () {
     	else if ($(this).children().hasClass('projects')) {
     		$('#content .projects-section').removeClass('hidden');
     	}
+		else if ($(this).children().hasClass('events')) {
+    		$('#content .events-section').removeClass('hidden');
+    	}
     	
+	});
+
 });
-});
+
+function makeRepoCards(json) {
+
+	section = document.getElementById("projects-section-column");
+
+	var card = document.createElement("div");
+
+	card.className = "card"
+	card.style = "width: 20rem; height: 10rem;"
+
+	var body = document.createElement("div");
+
+	body.className = "card-body"
+
+	var title = document.createElement("h5");
+
+	title.innerHTML = json["name"];
+	title.className = "card-title";
+
+	var text = document.createElement("p");
+
+	text.innerHTML = json["description"];
+	text.className = "card-text";
+
+	var link = document.createElement("a");
+
+	link.innerHTML = "Link";
+	link.href = json["html_url"];
+	link.className = "card-link btn btn-primary";
+
+	body.appendChild(title);
+	body.appendChild(text);
+	body.appendChild(link);
+
+	card.appendChild(body);
+
+	section.appendChild(card);
+
+}
